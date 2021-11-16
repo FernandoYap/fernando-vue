71cf09a6f26e5c68f3f6e922d456f201cb85b9f1
FernandoYap <94114516+FernandoYap@users.noreply.github.com>
2021-11-16 14:06:04 -0700

    install bootstrap

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="hello">
+
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm" style="background-color: darkcyan;" id="home">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="img1.jpg" alt="" width="40" height="40" class="d-inline-block align-text-top rounded-circle img-thumbnail" >
+      Fernando
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#projects">Tournaments</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<section class="jumbotron text-center">
+  <img src="img1.jpg" alt="Fernando" width="200" class="rounded-circle img-thumbnail">
+  <h1 class="display-4">Fernando</h1>
+  <p class="lead">Mahasiswa | Atlit Catur</p>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,256L720,64L1440,256L1440,320L720,320L0,320Z"></path></svg>
+</section>
+
+<section id="about">
+<div class="container">
+  <div class="row text-center fs-4 mb-2">
+    <div class="col">
+      <h2>About Me :</h2>
+      <p class="p">INDONESIA, Palembang</p>
+      <p class="p">Student at Multi Data Palembang University</p>
+      <p class="p">My Hobby is Playing Chess and Badminton</p>
+      <p class="p">Quote : Do not give up before trying</p>
+    </div>
+  </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e2edff" fill-opacity="1" d="M0,224L0,0L75.8,0L75.8,224L151.6,224L151.6,288L227.4,288L227.4,192L303.2,192L303.2,256L378.9,256L378.9,192L454.7,192L454.7,160L530.5,160L530.5,256L606.3,256L606.3,96L682.1,96L682.1,128L757.9,128L757.9,32L833.7,32L833.7,0L909.5,0L909.5,160L985.3,160L985.3,288L1061.1,288L1061.1,160L1136.8,160L1136.8,160L1212.6,160L1212.6,64L1288.4,64L1288.4,256L1364.2,256L1364.2,192L1440,192L1440,320L1364.2,320L1364.2,320L1288.4,320L1288.4,320L1212.6,320L1212.6,320L1136.8,320L1136.8,320L1061.1,320L1061.1,320L985.3,320L985.3,320L909.5,320L909.5,320L833.7,320L833.7,320L757.9,320L757.9,320L682.1,320L682.1,320L606.3,320L606.3,320L530.5,320L530.5,320L454.7,320L454.7,320L378.9,320L378.9,320L303.2,320L303.2,320L227.4,320L227.4,320L151.6,320L151.6,320L75.8,320L75.8,320L0,320L0,320Z"></path></svg>
+</section>
+
+<section id="projects">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+        <h2>Tournament</h2>
+      </div>
+    </div>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Kejuaraan Piala Bupati PALI</h5>
+        <p>Pali (Pendopo)</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Kejuaraan Nasional Catur Ke-47</h5>
+        <p>Banda Aceh</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Kejuaraan Daerah Sumatera Selatan</h5>
+        <p>Palembang</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+  </div>
+</section>
+
+<section id="contact">
+  <div class="row text-center">
+    <div class="col">
+      <h2>Contact Me</h2>
+      <p>Whatsapp  (0813 7985 2114)</p>
+      <p>Instagram (@fernando_yap)</p>
+      <p>LINE      (@fernando_1234)</p>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#008b8b" fill-opacity="1" d="M0,160L15,144C30,128,60,96,90,74.7C120,53,150,43,180,80C210,117,240,203,270,229.3C300,256,330,224,360,224C390,224,420,256,450,240C480,224,510,160,540,160C570,160,600,224,630,224C660,224,690,160,720,122.7C750,85,780,75,810,69.3C840,64,870,64,900,74.7C930,85,960,107,990,112C1020,117,1050,107,1080,90.7C1110,75,1140,53,1170,58.7C1200,64,1230,96,1260,133.3C1290,171,1320,213,1350,218.7C1380,224,1410,192,1425,176L1440,160L1440,320L1425,320C1410,320,1380,320,1350,320C1320,320,1290,320,1260,320C1230,320,1200,320,1170,320C1140,320,1110,320,1080,320C1050,320,1020,320,990,320C960,320,930,320,900,320C870,320,840,320,810,320C780,320,750,320,720,320C690,320,660,320,630,320C600,320,570,320,540,320C510,320,480,320,450,320C420,320,390,320,360,320C330,320,300,320,270,320C240,320,210,320,180,320C150,320,120,320,90,320C60,320,30,320,15,320L0,320Z"></path></svg>
+</section>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
